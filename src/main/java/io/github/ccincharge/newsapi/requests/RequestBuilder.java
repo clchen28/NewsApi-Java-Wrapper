71fc8747c29f039f9a6b7c772f162335b3ee8745
Funtoosh (Vivek) <vivek.funeesh@gmail.com>
2020-04-11 15:57:25 +0530

    Update RequestBuilder.java

--- src/main/java/io/github/ccincharge/newsapi/requests/RequestBuilder.java
+++ src/main/java/io/github/ccincharge/newsapi/requests/RequestBuilder.java
@@ -34,11 +34,15 @@ public class RequestBuilder {
     static private final Set<String> validCategories = Sets.newHashSet("business", "entertainment",
             "gaming", "general", "health-and-medical", "music", "politics", "science-and-nature",
             "sport", "technology");
-    static private final Set<String> validLanguages = Sets.newHashSet("ar", "en", "cn", "de", "es",
-            "fr", "he", "it", "nl", "no", "pt", "ru", "sv", "ud");
-    static private final Set<String> validCountries = Sets.newHashSet("ar", "au", "br", "ca", "cn",
-            "de", "es", "fr", "gb", "hk", "ie", "in", "is", "it", "nl", "no", "pk", "ru", "sa",
-            "sv", "us", "za");
+    static private final Set<String> validLanguages = Sets.newHashSet("AF", "AN", "AR", "AZ", "BG", "BN", 
+        "BR", "BS", "CA", "CS", "CY", "DA", "DE", "EL", "EN","EO", "ES", "ET", "EU", "FA", "FI", "FR", "GL", "HE", "HI",
+        "HR", "HT", "HU", "HY", "ID", "IS", "IT", "JP", "JV", "KK","KO", "LA", "LB", "LT", "LV", "MG", "MK", "ML", "MR",
+        "MS", "NL", "NN", "NO", "OC", "PL", "PT", "RO", "RU", "SH", "SK", "SL","SQ", "SR", "SV", "SW", "TA", "TE", "TH", 
+                                                                      "TL", "TR", "UK", "UR", "VI", "VO", "ZH");
+    static private final Set<String> validCountries = Sets.newHashSet("AE", "AR", "AT", "AU", "BE", "BG", "BR", "CA", 
+    "CH", "CN", "CO", "CU", "CZ", "DE", "EG", "FR", "GB", "GR", "HK", "HU", "ID", "IE", "IL", "IN", "IT", "JP", "KR", "LT", 
+    "LV", "MA", "MX", "MY", "NG", "NL", "NO", "NZ", "PH", "PL", "PT", "RO", "RS", "RU", "SA", "SE", "SG", "SI", "SK", "TH", 
+                                                                      "TR", "TW", "UA", "US", "VE", "ZA");
     static private final Set<String> validSortBy = Sets.newHashSet("relevancy", "popularity",
             "publishedAt");
 
